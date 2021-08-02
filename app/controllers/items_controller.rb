b231d50f37d781b9b8f6ee39efcabbbd06555f77
wakame-se <jonybeans18@gmail.com>
2021-08-02 20:46:59 +0900

    JSによる販売手数料・販売利益の計算結果表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,7 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
+      render json: {item: item}
     else
       render :new
     end
