76930818a27e993944c5c936a287d5345988a80e
wakame-se <jonybeans18@gmail.com>
2021-08-05 14:52:38 +0900

    エラーメッセージの表示

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,12 +5,12 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "input correctly"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "select"}
     validates :city
     validates :house_number
-    validates :telephone_number, format: {with: /\A0[0-9]{9,10}\z/, message: "is invalid. Input half-width numbers."}
+    validates :telephone_number, format: {with: /\A0[0-9]{9,10}\z/, message: "input only half-width numbers"}
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
