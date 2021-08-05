16f3fbe02aeca274fd2e951393fc4e2da9298451
wakame-se <jonybeans18@gmail.com>
2021-08-05 14:23:48 +0900

    sold outの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :item_find, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order(created_at: :DESC)
@@ -50,7 +50,7 @@ class ItemsController < ApplicationController
                                  :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 
-  def ensure_correct_user
-    redirect_to root_path unless @item.user_id == current_user.id
+  def move_to_index
+    redirect_to root_path if @item.user_id != current_user.id || @item.order.present?
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_index
 
   def index
     @order_address = OrderAddress.new
@@ -21,5 +22,10 @@ class OrdersController < ApplicationController
   def order_params
     @item = Item.find(params[:item_id])
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if @item.order.present? || @item.user_id == current_user.id
   end
 end
